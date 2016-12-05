11f6e0827210452f8479a8f04149d695c32ae7e5
Cody Lynch <lynchco@sonoma.edu>
2016-12-04 21:48:03 -0800

    Remove partial triangle summation and switch to outputting a CSV file of cartesian coordinates.

--- ArduinoPondArea.ino
+++ ArduinoPondArea.ino
@@ -1,68 +1,164 @@
-/*
-  AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
-  Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
 
-  This example code is in the public domain.
-*/
+// @ 40 ticks per 360 degree revolution
+// 1 meter of rope = rotary aparatus circumference
+const float rotaryRopeMetersPerTick = 1.0 * 360.0/40.0;
 
-// starting @ a tick position and going to another tick position,
-// returns how many degrees we've moved.
-float degreeChange(int fromTick, int toTick);
+int pinA = 3;  // Connected to CLK on KY-040
+int pinB = 4;  // Connected to DT on KY-040
+
+int rotaryTicks = 0;
+int lastRotaryTicks = 0;
+
+int lastRotaryValue; // 1/0 read from rotary
+
+boolean rotatingClockwise;
+
+// Given a tick value from the potent, return an angle
+float potentTick2Angle(int potentTickCount);
 
 
 // set on init so we know what the orientation "0" is
 int initialPotentiometerReading = -1;
 
-void writeCsvLabelLine();
-void writeCsvDataLine(int rotaryTicks, int angleTicks, );
+int lastPotentiometerValue = -1; // cant be -1
 
-// the setup routine runs once when you press reset:
-void setup() {
-  // initialize serial communication at 9600 bits per second:
-  Serial.begin(9600);
-  initialPotentiometerReading = analogRead(A0);
-}
+// smooths out potentiometer readingts b/c jitter sucks
+void addNewPoteniometerPointSample(int newPotentValue);
+int rollingAveragePotent();
 
-int lastSensorValue = -1; // cant be this
 
-// the loop routine runs over and over again forever:
-void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A0);
-  // look until change
-  if (sensorValue == lastSensorValue) {
-    return; // reloop
-  }
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+// util functions for writing in CSV format
+void writeCsvLabel();
+void writeCsvRow(int rotaryTickCount, int potentTickCount, double x, double y);
+
+
+// hacky because for some reason we can print in the setup() fxn
+bool wroteCsvHeader;
  
-  // print out the value you read:
-  Serial.print(sensorValue);
-  Serial.print(" est. angle delta = ");
-  Serial.print(degreeChange(lastSensorValue, sensorValue));
+void setup() { 
+    pinMode (pinA, INPUT);
+    pinMode (pinB, INPUT);
+    /* Read Pin A
+    Whatever state it's in will reflect the last position   
+    */
+    lastRotaryValue = digitalRead(pinA);
+    initialPotentiometerReading = analogRead(A0);
+    wroteCsvHeader = false;
+    Serial.begin (9600);
+    Serial.println("Now running!");
+}
 
-  Serial.print(" total <) ");
-  Serial.print(degreeChange(initialPotentiometerReading, sensorValue));
+ void loop() { 
+  // if they disconnect from reading the output stream, resend the header data on reconnec
+  if (!Serial) {
+    wroteCsvHeader = false;
+  }
+
+  if (!wroteCsvHeader && Serial) // if(Serial) only available on Leonardo: detects if tty connected
+  {
+    delay(150); // 150ms for the client to prepare for display cause clients were slow & dropping label packets
+    writeCsvLabel();
+    wroteCsvHeader = true;
+  }
   
-  Serial.println("");
+   int newRotaryValue = digitalRead(pinA);
+   addNewPoteniometerPointSample(analogRead(A0));
+   int newPotentiometerValue = rollingAveragePotent();
 
-  lastSensorValue = sensorValue;
-  delay(500);        // delay in between reads for stability
+
+   // we only care when the nob rotates. If not, re-loop
+   if (newRotaryValue == lastRotaryValue && abs(newPotentiometerValue-lastPotentiometerValue) < 5) // jitter
+   {
+      return; // reloop
+   }
+  
+   if (newRotaryValue != lastRotaryValue) {
+    // if the knob is rotating, we need to determine direction
+    // We do that by reading pin B.
+    lastRotaryTicks = rotaryTicks;
+    if (digitalRead(pinB) != newRotaryValue) {  // Means pin A Changed first - We're Rotating Clockwise
+     rotaryTicks++;
+     rotatingClockwise = true;
+    } else {// Otherwise B changed first and we're moving CCW
+     rotatingClockwise = false;
+     rotaryTicks--;
+    }
+   }
+
+   
+    Serial.print("rotary="); Serial.print(rotaryTicks);
+    Serial.print(", potent = "); Serial.println(newPotentiometerValue);
+
+    // get our coordinates in polar
+    float angle = potentTick2Angle(newPotentiometerValue);
+    float distance = rotaryRopeMetersPerTick * rotaryTicks;
+
+    // convert polar coordinates to cartesian
+    float x = distance * cos(angle);
+    float y = distance * sin(angle);
+
+    writeCsvRow(rotaryTicks, newPotentiometerValue, x, y);
+
+    // ================================================
+    // end of loop
+    lastRotaryValue = newRotaryValue;
+    lastPotentiometerValue = newPotentiometerValue;
+ } 
+
+
+float potentTick2Angle(int potentTickCount) {
+  // So it was found that the mapping from potent ticks to angles is not linear
+  // its exponential.
+  // We fit a curve with mathematica by using 
+  // "fit  {{90, 55}, {180, 280}, {270, 1023}} exponential"
+  // and it spat out
+  // angle = 18.9994 e^(0.0147675 tickCount)
+  // but that has a y intercept of ~18 degrees, so we chose to hand fit
+  // a curve (being dangerous!) of the form
+  // angle = y=a log(1+x) + b sqrt(x) for x = stepcount
+  // and chose a = 24 and b=7.1 which has good fit in our critical area of 0-180 degrees
+  const double a = 24.0;
+  const double b = 7.1;
+  const double e = 2.718281828459045;
+  return a*log10(1+potentTickCount) + b*sqrt(potentTickCount);
 }
 
 
-float degreeChange(int fromTick, int toTick) {
-  // this is the real world mapping of the degrees we've turned it to the internal
-  // tick count. We suspect this is actually non-linear, which we'll fix later.
-  const float realDegreesPerTicks = 30.0/700.00; // 30 deg turn = 0 to 700 tick
+// sometimes we get stuck between two discrete values
+const int JITTER_SAMPLES_COUNT = 4;
+int potentData[JITTER_SAMPLES_COUNT];
+int jitter_pointer = 0;
 
-  // right now we're using a linear mapping of ticks to degrees,
-  // but we'll change this later here if we need to
-  int tickChange = toTick-fromTick; 
-  float degChange = tickChange * realDegreesPerTicks;
+void addNewPoteniometerPointSample(int newPotentValue)
+{
+  potentData[jitter_pointer++ % JITTER_SAMPLES_COUNT] = newPotentValue;
+  jitter_pointer = jitter_pointer%JITTER_SAMPLES_COUNT;
+}
 
-  // we always want our degree output to be positive, so abs it
-  return abs(degChange);
+int rollingAveragePotent()
+{
+  int sum = 0;
+  for(int i = 0; i < JITTER_SAMPLES_COUNT; i++) sum += potentData[i];
+  return sum/JITTER_SAMPLES_COUNT;
+}
+
+// And now for CSV utility functions
+void writeCsvLabel()
+{
+  // we write the read sensor values first to preserve the raw data and then our interpreted/calculated x,y points
+  // in the cartesian plane
+  // so it's (r, theta, x, y)
+  Serial.println("LABEL,RotaryTickCount r (arb),PotentiometerTickCount theta (arb),Calc X (meters), Calc Y (meters)");
+}
+
+void writeCsvRow(int rotaryTickCount, int potentTickCount, double x, double y)
+{
+  // these need to be in the same order as writeCsvLabel()'s implementation
+  Serial.print("DATA");
+  Serial.print(","); Serial.print(rotaryTickCount);
+  Serial.print(","); Serial.print(potentTickCount);
+  Serial.print(","); Serial.print(x);
+  Serial.print(","); Serial.print(y);
+  Serial.println();
 }
 
